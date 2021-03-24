61693a2fce781581360f2730a7e6c524f94f71df
venkateshnichenametla <nichanametlavenkatesh@gmail.com>
2021-03-24 16:21:14 +0530

    Avoided duplications and included missing testcases

--- SensorValidate.Tests/SensorValidatorTest.cs
+++ SensorValidate.Tests/SensorValidatorTest.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 
 using Xunit;
@@ -8,16 +7,38 @@ namespace SensorValidate.Tests
     public class SensorValidatorTest
     {
         [Fact]
-        public void reportsErrorWhenSOCjumps() {
+        public void reportsErrorWhenSOCjumps() 
+        {
             Assert.False(SensorValidator.validateSOCreadings(
-                new List<double>{0.0, 0.01, 0.5, 0.51}
+                new List<double>{0.0, 0.01, 0.5, 0.51}, 0.05    
             ));
         }
         [Fact]
-        public void reportsErrorWhenCurrentjumps() {
+        public void reportsErrorWhenCurrentjumps() 
+        {
             Assert.False(SensorValidator.validateCurrentreadings(
-                new List<double>{0.03, 0.03, 0.03, 0.33}
+                new List<double>{0.03, 0.03, 0.03, 0.33}, 0.1
             ));
+        }
+        [Fact]
+        public void NoErrorWhenCurrentJumps()
+        {
+            Assert.True(SensorValidator.validateCurrentreadings(
+                new List<double> { 0.03, 0.08, 0.15, 0.21 }, 0.1
+            ));
+        }
+        [Fact]
+        public void NoErrorWhenSOCJumps()
+        {
+            Assert.True(SensorValidator.validateSOCreadings(
+                new List<double> { 0.0, 0.01, 0.05, 0.08 }, 0.05
+            ));
+        }
+
+        [Fact]
+        public void reportsErrorWhenNull()
+        {
+            Assert.False(SensorValidator.validateCurrentreadings(null, 0.05));
         }
     }
 }
